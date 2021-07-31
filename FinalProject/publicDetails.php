950097a8fc5220518a45423b99634c238e004921
Ananya <Ananyachy2001@gmail.com>
2021-07-31 21:07:59 +0600

    25%

--- FinalProject/publicDetails.php
+++ FinalProject/publicDetails.php
@@ -11,19 +11,36 @@
 
 
     <body>
-        <h1>Details</h1>
+        <h3><a href="#">Home</a></h3>
         <h1 align="center">Welcome <?php echo $_SESSION["loggedpublic"];?></h1>
 
         <table>
-    <th>
-        <td>S1#</td>
-        <td>Name</td>
-        <td>Product</td>
-    </th>
+    <tr>
+        <td><a href="#">=>Click Here for Latest News on Covid-19</a></td>
+
+    </tr>
 
     <tr>
+    <td><a href="vaccineAvailability.php">=>Click Here If Vaccine is Available or Not </a></td>
+    </tr>
 
+    <tr>
+    <td><a href="#">=>Check Your Vaccine Registration Status</a></td>
+    </tr>
 
+    <tr>
+    <td><a href="#">=>Download Your Vaccine registration</a></td>
+    </tr>
+    <tr>
+    <td><a href="#">=>Download Your Vaccine certificate</a></td>
+    </tr>
+
+    <tr>
+    <td><a href="#">=>Verify Your Vaccination Certificate</a></td>
+    </tr>
+
+    <tr>
+    <td><a href="#">=>Can give feedback</a></td>
     </tr>
 
 
---
+++ FinalProject/vaccineAvailability.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'controllers/publicController.php';
+require_once 'controllers/vaccineController.php';
+
+?>
+
+<html>
+    <head></head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <body>
+        
+    <h3><a href="#">Home</a></h3>
+    
+<form  method='post'>
+
+
+<h2>Check Vaccine is availble or not: <input type="submit" id='sp1' name="check" value="Check"></h2>
+
+
+</form>
+
+    
+
+
+
+
+    </body>
+</html>
